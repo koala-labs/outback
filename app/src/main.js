160683f3fc687604f299359cd07f4f85ad856eaa
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-10-02 11:22:00 -0400

    [Frontend] Setup vue-progressbar

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueProgressBar from 'vue-progressbar';
 import App from './App';
 import router from './router';
 import store from './store';
 
+const progressBarOptions = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300,
+  },
+  autoRevert: true,
+  location: 'left',
+  inverse: false,
+};
+
+Vue.use(VueProgressBar, progressBarOptions);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
