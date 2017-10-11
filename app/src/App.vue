0d604f7a315c947b25d1f44d4cb295642afa32d2
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-10-11 15:43:07 -0400

    Add custom notification syling

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <router-view></router-view>
     <vue-progress-bar></vue-progress-bar>
-    <notifications group="ufo" position="top right"/>
+    <notifications group="ufo"
+                   classes="n-light"
+                   :max="3"
+                   :width="350"/>
   </div>
 </template>
 
@@ -12,7 +15,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang='scss'>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,5 +23,47 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 10px;
+}
+
+.notification.n-light {
+  margin: 0.5rem 0.5rem 1rem;
+  border-radius: 2px;
+
+  font-size: 14px;
+  font-weight: bold;
+
+  padding: 10px 20px;
+
+  color: #03a9f4;
+  background: #E1F5FE;
+
+  transition: box-shadow .25s;
+  
+  box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
+
+  &.error {
+    background: #FFEBEE;
+    color: #F44336;
+
+    .notification-title {
+      color: #F44336;
+    }
+  }
+
+  &.success {
+    background: #E8F5E9;
+    color: #4CAF50;
+
+    .notification-title {
+      color: #4CAF50;
+    }
+  }
+
+  .notification-title {
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-size: 14px;
+    color: #03a9f4;
+  }
 }
 </style>
