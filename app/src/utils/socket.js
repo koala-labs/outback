e0b20f5cfa717c20b703dd6a2652f596e1578ed7
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-12-15 23:34:56 -0500

    Remove vue app and gin server

--- app/src/utils/socket.js
+++
@@ -1,7 +0,0 @@
-import API from '@/constants/api';
-
-const socket = ({ cluster, service }) => {
-  return new WebSocket(`${API.ws}${API.url}${API.routes.status}?cluster=${cluster}&service=${service}`);
-};
-
-export default socket;
