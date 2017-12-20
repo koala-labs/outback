ea133664009ca568ba77db8570e4a368617847ac
Kelvin Acosta <kacosta@fuzzproductions.com>
2017-12-20 12:29:24 -0500

    [Core] Add tests

--- ufo-core/ufo_test.go
+++ ufo-core/ufo_test.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/ecr"
+
 	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/aws/aws-sdk-go/service/ecr/ecriface"
@@ -24,6 +26,19 @@ type logger struct{}
 
 func (l *logger) Printf(format string, a ...interface{}) {
 	fmt.Printf(format, a...)
+}
+
+func (m mockECRClient) DescribeImages(s *ecr.DescribeImagesInput) (*ecr.DescribeImagesOutput, error) {
+	var tag1, tag2 string = "tag1", "tag2"
+	return &ecr.DescribeImagesOutput{
+		ImageDetails: []*ecr.ImageDetail{
+			&ecr.ImageDetail{
+				ImageTags: []*string{&tag1},
+			},
+			&ecr.ImageDetail{
+				ImageTags: []*string{&tag2},
+			}},
+	}, nil
 }
 
 func (m mockECSClient) DescribeClusters(s *ecs.DescribeClustersInput) (*ecs.DescribeClustersOutput, error) {
@@ -50,7 +65,7 @@ func (m mockECSClient) DescribeTaskDefinition(s *ecs.DescribeTaskDefinitionInput
 	fam := "family1"
 	command := "echo"
 	commands := []*string{&command, &command}
-	image := "111222333444.dkr.ecr.us-west-1.amazonaws.com/test"
+	image := "111222333444.dkr.ecr.us-west-1.amazonaws.com/image:latest"
 	return &ecs.DescribeTaskDefinitionOutput{
 		TaskDefinition: &ecs.TaskDefinition{
 			ContainerDefinitions: []*ecs.ContainerDefinition{&ecs.ContainerDefinition{
@@ -59,6 +74,17 @@ func (m mockECSClient) DescribeTaskDefinition(s *ecs.DescribeTaskDefinitionInput
 			}},
 			Family: &fam,
 		},
+	}, nil
+}
+
+func (m mockECSClient) DescribeTasks(s *ecs.DescribeTasksInput) (*ecs.DescribeTasksOutput, error) {
+	lastStatus := "PENDING"
+	taskDefArn := "111222333444.dkr.ecr.us-west-1.amazonaws.com/task"
+	return &ecs.DescribeTasksOutput{
+		Tasks: []*ecs.Task{&ecs.Task{
+			LastStatus:        &lastStatus,
+			TaskDefinitionArn: &taskDefArn,
+		}},
 	}, nil
 }
 
@@ -252,11 +278,11 @@ func TestUFOGetService(t *testing.T) {
 	}
 }
 
-func TestUfoGetTaskDefinition(t *testing.T) {
+func TestUFOGetTaskDefinition(t *testing.T) {
 	fam := "family1"
 	command := "echo"
 	commands := []*string{&command, &command}
-	image := "111222333444.dkr.ecr.us-west-1.amazonaws.com/test"
+	image := "111222333444.dkr.ecr.us-west-1.amazonaws.com/image:latest"
 	cases := []struct {
 		Expected *ecs.TaskDefinition
 	}{
@@ -286,10 +312,98 @@ func TestUfoGetTaskDefinition(t *testing.T) {
 		if a, e := *taskDef.Family, *c.Expected.Family; a != e {
 			t.Errorf("%d, expected %v family, got %v", i, e, a)
 		}
+		if a, e := *taskDef.ContainerDefinitions[0].Image, *c.Expected.ContainerDefinitions[0].Image; a != e {
+			t.Errorf("%d, expected %v image, got %v", i, e, a)
+		}
 		actualContainerCommand := strings.Join(aws.StringValueSlice(taskDef.ContainerDefinitions[0].Command), " ")
 		expectedContainerCommand := strings.Join(aws.StringValueSlice(c.Expected.ContainerDefinitions[0].Command), " ")
 		if a, e := actualContainerCommand, expectedContainerCommand; a != e {
 			t.Errorf("%d, expected %v command, got %v", i, e, a)
+		}
+	}
+}
+
+func TestUFOGetTasks(t *testing.T) {
+	var lastStatus, taskDefArn string = "PENDING", "111222333444.dkr.ecr.us-west-1.amazonaws.com/task"
+	cases := []struct {
+		Expected *ecs.DescribeTasksOutput
+	}{
+		{
+			Expected: &ecs.DescribeTasksOutput{
+				Tasks: []*ecs.Task{
+					&ecs.Task{
+						LastStatus:        &lastStatus,
+						TaskDefinitionArn: &taskDefArn,
+					}},
+			},
+		},
+	}
+
+	ufo := mockUFO()
+
+	clusters, err := ufo.Clusters()
+	cluster, err := ufo.GetCluster(clusters[0])
+	services, err := ufo.Services(cluster)
+	service, err := ufo.GetService(cluster, services[0])
+	runningTasks, err := ufo.RunningTasks(cluster, service)
+	tasks, err := ufo.GetTasks(cluster, runningTasks)
+
+	for i, c := range cases {
+		if err != nil {
+			t.Fatalf("%d, unexpected error", err)
+		}
+		if a, e := len(tasks.Tasks), len(c.Expected.Tasks); a != e {
+			t.Fatalf("%d, expected %d tasks, got %d", i, e, a)
+		}
+		for j, task := range tasks.Tasks {
+			if a, e := task, c.Expected.Tasks[j]; *a.LastStatus != *e.LastStatus {
+				t.Errorf("%d, expected %v LastStatus, got %v", i, e, a)
+			}
+			if a, e := task, c.Expected.Tasks[j]; *a.TaskDefinitionArn != *e.TaskDefinitionArn {
+				t.Errorf("%d, expected %v TaskDefinitionARN, got %v", i, e, a)
+			}
+		}
+	}
+}
+
+func TestUFOGetImages(t *testing.T) {
+	var tag1, tag2 string = "tag1", "tag2"
+	cases := []struct {
+		Expected []*ecr.ImageDetail
+	}{
+		{
+			Expected: []*ecr.ImageDetail{
+				&ecr.ImageDetail{
+					ImageTags: []*string{&tag1},
+				},
+				&ecr.ImageDetail{
+					ImageTags: []*string{&tag2},
+				}},
+		},
+	}
+
+	ufo := mockUFO()
+
+	clusters, err := ufo.Clusters()
+	cluster, err := ufo.GetCluster(clusters[0])
+	services, err := ufo.Services(cluster)
+	service, err := ufo.GetService(cluster, services[0])
+	taskDef, err := ufo.GetTaskDefinition(cluster, service)
+	images, err := ufo.GetImages(taskDef)
+
+	for i, c := range cases {
+		if err != nil {
+			t.Fatalf("%d, unexpected error", err)
+		}
+		if a, e := len(images), len(c.Expected); a != e {
+			t.Fatalf("%d, expected %d tasks, got %d", i, e, a)
+		}
+		for j, image := range images {
+			actualImageTag := strings.Join(aws.StringValueSlice(image.ImageTags), " ")
+			expectedImageTag := strings.Join(aws.StringValueSlice(c.Expected[j].ImageTags), " ")
+			if a, e := actualImageTag, expectedImageTag; a != e {
+				t.Errorf("%d, expected %v TaskDefinitionARN, got %v", i, e, a)
+			}
 		}
 	}
 }
