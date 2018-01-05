96720a15e5b28d4cf1cbe61285f2dc9c6785812d
Kelvin Acosta <kacosta@fuzzproductions.com>
2018-01-05 12:09:36 -0500

    Run command either from its shortcut in the config or directly from the flag

--- cmd/ufo/task_run.go
+++ cmd/ufo/task_run.go
@@ -2,16 +2,15 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 	UFO "gitlab.fuzzhq.com/Web-Ops/ufo/ufo"
 )
 
 var (
-	flagTaskCluster         string
-	flagTaskService         string
-	flagTaskCommandOverride string
+	flagTaskCluster string
+	flagTaskService string
+	flagTaskCommand string
 )
 
 var taskRunCmd = &cobra.Command{
@@ -22,24 +21,28 @@ var taskRunCmd = &cobra.Command{
 }
 
 func taskRun(cmd *cobra.Command, args []string) {
-	env, err := getSelectedEnv(flagTaskCluster)
+	c, err := cfg.getSelectedCluster(flagTaskCluster)
 
+	handleError(err)
+
+	service, err := cfg.getSelectedService(c.Services, flagTaskService)
+
+	handleError(err)
+
+	// Check if the command is available in the config as a shortcut
+	command, err := cfg.getCommand(flagTaskCommand)
+
+	// If the shortcut is not in the config, pass the command directly
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-		os.Exit(1)
+		err = run(c.Name, *service, flagTaskCommand)
+	} else {
+		err = run(c.Name, *service, *command)
 	}
 
-	service, err := getSelectedService(env.Services, flagTaskService)
-
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		os.Exit(1)
-	}
-
-	run(env.Cluster, *service)
+	handleError(err)
 }
 
-func run(cluster string, service string) error {
+func run(cluster string, service string, command string) error {
 	ufo := UFO.New(ufoCfg)
 	c, err := ufo.GetCluster(cluster)
 
@@ -59,13 +62,13 @@ func run(cluster string, service string) error {
 		return err
 	}
 
-	_, err = ufo.RunTask(c, t, flagTaskCommandOverride)
+	_, err = ufo.RunTask(c, t, command)
 
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Running task...")
+	fmt.Printf("Running task with command %s", command)
 
 	return nil
 }
@@ -76,5 +79,6 @@ func init() {
 
 	taskRunCmd.Flags().StringVarP(&flagTaskCluster, "cluster", "c", "", "cluster")
 	taskRunCmd.Flags().StringVarP(&flagTaskService, "service", "s", "", "service to use base image from")
-	taskRunCmd.Flags().StringVarP(&flagTaskCommandOverride, "override", "o", "", "command to override")
+	taskRunCmd.Flags().StringVarP(&flagTaskCommand, "command", "n", "", "name of the command to run from your config or the command itself")
+
 }
