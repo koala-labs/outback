b62f88509185817866278634915f2616896fbb04
Kelvin Acosta <kacosta@fuzzproductions.com>
2018-02-12 15:41:02 -0500

    Add docs for service command

--- cmd/ufo/service.go
+++ cmd/ufo/service.go
@@ -7,6 +7,11 @@ import (
 var serviceCmd = &cobra.Command{
 	Use:   "service",
 	Short: "Manage an ECS service",
+	Long: `Services manage long-lived instances of your containers that are run on AWS
+	ECS. If your container exits for any reason, the service scheduler will
+	restart your containers and ensure your service has the desired number of
+	tasks running. Services can be used in concert with a load balancer to
+	distribute traffic amongst the tasks in your service.`,
 }
 
 func init() {
