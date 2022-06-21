70537cf2e79bf563256c46bafc6d54d560b14a66
walter <walter.beller.morales@gmail.com>
2022-06-21 10:05:14 -0400

    update docker pkg to support pulling images

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -2,6 +2,7 @@ package docker
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/koala-labs/outback/pkg/term"
@@ -9,32 +10,33 @@ import (
 
 // ImageBuild builds a docker image based on the configured dockerfile for
 // the cluster you are deploying to and tags the image with the vcs head
-func ImageBuild(repo string, tag string, dockerfile string, buildArgs []string, configBuildArgs []string) error {
+func ImageBuild(repo string, tag string, dockerfile string, buildArgs []string, configBuildArgs []string, cacheFrom []string) error {
 	image := fmt.Sprintf("%s:%s", repo, tag)
+	combinedBuildArgs := append(configBuildArgs, buildArgs...)
+	// ensure built images are optimized for remote caching
+	combinedBuildArgs = append(combinedBuildArgs, "BUILDKIT_INLINE_CACHE=1")
 
-	dockerConfigBuildArgs := make([]string, 2*len(configBuildArgs), 2*len(configBuildArgs))
-	dockerBuildArgs := make([]string, 2*len(buildArgs), 2*len(buildArgs))
+	dockerCmdBuildArgs := make([]string, 2*(len(combinedBuildArgs)), 2*(len(combinedBuildArgs)))
+	dockerCacheFromArgs := make([]string, 2*len(cacheFrom), 2*len(cacheFrom))
 
-	for i, v := range configBuildArgs {
-
-		dockerConfigBuildArgs[i*2] = "--build-arg"
-		dockerConfigBuildArgs[i*2+1] = v
-
+	for i, v := range combinedBuildArgs {
+		dockerCmdBuildArgs[i*2] = "--build-arg"
+		dockerCmdBuildArgs[i*2+1] = v
 	}
 
-	for i, v := range buildArgs {
-
-		dockerBuildArgs[i*2] = "--build-arg"
-		dockerBuildArgs[i*2+1] = v
-
+	for i, v := range cacheFrom {
+		dockerCacheFromArgs[i*2] = "--cache-from"
+		dockerCacheFromArgs[i*2+1] = v
 	}
 
 	dockerCmd := "docker"
 	dockerCmdArgs := []string{"build", "-f", dockerfile, "-t", image, "."}
-	dockerCmdBuildArgs := append([]string(dockerConfigBuildArgs), []string(dockerBuildArgs)...)
-	dockerCmdFullArgs := append([]string(dockerCmdArgs), []string(dockerCmdBuildArgs)...)
+	dockerCmdFullArgs := append(dockerCmdArgs, dockerCmdBuildArgs...)
+	dockerCmdFullArgs = append(dockerCmdFullArgs, dockerCacheFromArgs...)
 
 	cmd := exec.Command(dockerCmd, dockerCmdFullArgs...)
+	// enable BuildKit: https://docs.docker.com/engine/reference/builder/#buildkit
+	cmd.Env = append(os.Environ(), "DOCKER_BUILDKIT=1")
 
 	if err := term.PrintStdout(cmd); err != nil {
 		return ErrImageBuild
@@ -51,6 +53,19 @@ func ImagePush(repo string, tag string) error {
 
 	if err := term.PrintStdout(cmd); err != nil {
 		return ErrImagePush
+	}
+
+	return nil
+}
+
+// ImagePull pulls an image with a specific tag from the configured repository
+func ImagePull(repo string, tag string) error {
+	image := fmt.Sprintf("%s:%s", repo, tag)
+
+	cmd := exec.Command("docker", "pull", image)
+
+	if err := term.PrintStdout(cmd); err != nil {
+		return ErrImagePull
 	}
 
 	return nil
